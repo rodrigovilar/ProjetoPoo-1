bd9bacc9b8f0130d3aa437a3b8c93e6413a61414
EduardoE <eduardo_bikke@hotmail.com>
2014-02-25 15:09:15 -0300

    A

--- src/br/ufpb/dce/lcc/poo/sistemademercado/SistemaDeMercadoTest.java
+++ src/br/ufpb/dce/lcc/poo/sistemademercado/SistemaDeMercadoTest.java
@@ -19,20 +19,23 @@ public class SistemaDeMercadoTest {
 	@Test
 	public void cadastrarFuncionarioNoSistema () {
 		sistema.cadastraFuncionario("edu","12345");
-		
+		assertNotNull(sistema.pesquisaFuncionarioPeloNome("edu"));
 	}
 	@Test
 	public void removerFuncionarioDoSistemaPorCPF () {
 		sistema.cadastraFuncionario("edu","12345");
 		sistema.removeFuncionarioPorCPF("12345");
+		assertNull (sistema.pesquisaFuncionarioPeloNome("edu"));
 	}
 	@Test
 	public void pesquisarFuncionarioNoSistemaPeloNome () {
-		sistema.pesquisaFuncionarioPeloNome("edu");
+		this.cadastrarFuncionarioNoSistema();
+		assertNotNull(sistema.pesquisaFuncionarioPeloNome("edu"));
 	}
 	@Test
 	public void pesquisarFuncionarioNoSistemaPeloCPF () {
-		 sistema.pesquisaFuncionarioPeloCPF ("12345");
+		this.cadastrarFuncionarioNoSistema(); 
+		assertNotNull(sistema.pesquisaFuncionarioPeloCPF("12345"));
 	}
 	@Test (expected = ExcecaoSistemaDeMercado.class)
 	public void cadastrarMesmoFuncionarioNoSistema () {
